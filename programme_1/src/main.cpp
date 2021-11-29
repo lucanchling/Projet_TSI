69207af5976754060a1a0dc4bd36cf3cde256bcb
Luc Anchling <luc.anchling@cpe.fr>
2021-11-29 09:21:21 +0100

    affichage triangle

--- programme_1/src/main.cpp
+++ programme_1/src/main.cpp
@@ -31,7 +31,10 @@
  * 
  \*****************************************************************************/
 double time_count = 0; //Compteur de temps (en s)
-int delta_t = 100; //Variation de temps (en ms) 
+int delta_t = 100; //Variation de temps (en ms)
+/*****************************************************************************/
+unsigned int vao, vbo; 
+
 //identifiant du shader
 GLuint shader_program_id;
 
@@ -40,6 +43,9 @@ GLuint shader_program_id;
  \*****************************************************************************/
 static void init()
 {
+  float sommets[]={ 0.0f,0.0f,0.0f,
+                    1.0f,0.0f,0.0f,
+                    0.0f,1.0f,0.0f};
   // Chargement du shader
   shader_program_id = glhelper::create_program_from_file(
       "programme_1/src/shader.vert", 
@@ -48,15 +54,40 @@ static void init()
 
   //activation de la gestion de la profondeur
   glEnable(GL_DEPTH_TEST); CHECK_GL_ERROR();
+
+  //attribution d'une liste d' ́etat (1 indique la cr ́eation d'une seule liste)
+  glGenVertexArrays(1, &vao);
+  //affectation de la liste d' ́etat courante
+  glBindVertexArray(vao);
+  //attribution d’un buffer de donnees (1 indique la cr ́eation d’un seul buffer)
+  glGenBuffers(1,&vbo); CHECK_GL_ERROR();
+  //affectation du buffer courant
+  glBindBuffer(GL_ARRAY_BUFFER,vbo); CHECK_GL_ERROR();
+  //copie des donnees des sommets sur la carte graphique
+  glBufferData(GL_ARRAY_BUFFER,sizeof(sommets),sommets,GL_STATIC_DRAW);
+  CHECK_GL_ERROR();
+  // Les deux commandes suivantes sont stock ́ees dans l' ́etat du vao courant
+  // Active l'utilisation des donn ́ees de positions
+  // (le 0 correspond `a la location dans le vertex shader)
+  glEnableVertexAttribArray(0); CHECK_GL_ERROR();
+  // Indique comment le buffer courant (dernier vbo "bind ́e")
+  // est utilis ́e pour les positions des sommets
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0); CHECK_GL_ERROR();
 }
 
 //Fonction d'affichage
 static void display_callback()
 {
   //effacement des couleurs du fond d'ecran
-  glClearColor(abs(sin(time_count*2*M_PI)), abs(cos(time_count*2*M_PI)), abs(0.5*sin(time_count*2*M_PI)+0.5*cos(time_count*2*M_PI)), 1.0f); CHECK_GL_ERROR();
+  //glClearColor(abs(sin(time_count*2*M_PI)), abs(cos(time_count*2*M_PI)), abs(0.5*sin(time_count*2*M_PI)+0.5*cos(time_count*2*M_PI)), 1.0f); 
+  glClearColor(0.3f, 0.2f, 0.6f, 1.0f);CHECK_GL_ERROR();
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); CHECK_GL_ERROR();
-
+  //glDrawArrays(GL_LINE_LOOP, 0, 3); CHECK_GL_ERROR();
+  //GL_LINE_LOOP trace que le contour du triangle sinon GL_TRIANGLES affiche tout le triangle même l'intérieur
+  glPointSize(5.0);
+  glDrawArrays(GL_POINTS, 0, 3);
+  glDrawArrays(GL_LINE_LOOP, 0, 3);
+  CHECK_GL_ERROR();
   //Changement de buffer d'affichage pour eviter un effet de scintillement
   glutSwapBuffers();
 }
